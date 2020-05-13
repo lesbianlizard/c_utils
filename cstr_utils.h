cc1bad474435a2ba55fca3f98dd2fefa42460c99
Reed Reese-Steiner <arleycat@gmail.com>
2020-05-12 22:44:35 -0700

    add mmap_file_cstr from gl_thing

--- cstr_utils.h
+++ cstr_utils.h
@@ -1,6 +1,8 @@
 #ifndef CSTR_UTILS_H
 #define CSTR_UTILS_H
 
+// Concatenate two C strings, dynamically allocating memory for the result
+// FIXME: do something different for the count variable
 char *
 cstrcat(char* str1, char* str2, size_t count)
 {
@@ -29,6 +31,30 @@ cstrcat(char* str1, char* str2, size_t count)
   }
 
   return result;
+}
+
+// Memory map a file to a C string. Don't forget to close(fd_ret) when you're done with it!
+int
+mmap_file_cstr(char *name, int *fd_ret, char **cstr_ret)
+{
+  int fd = open(name, O_RDONLY);
+
+  if (fd > 0)
+  {
+    int len = lseek(fd, 0, SEEK_END);
+    *cstr_ret = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
+    *fd_ret = fd;
+    return 0;
+  }
+  else
+  {
+    fprintf(stderr, _("[%1$s] error %2$i opening file '%3$s': %4$s\n"),
+      __func__,
+      errno,
+      name,
+      strerror(errno));
+    return -1;
+  }
 }
 
 #endif // CSTR_UTILS_H
