b315366457a6d297b60bee46a9909121326ce494
Reed Reese-Steiner <arleycat@gmail.com>
2022-02-22 00:55:01 -0600

    implement a bunch of 3d vector functions:

--- maths.c
+++ maths.c
@@ -9,11 +9,27 @@ dotp2d(struct vec2d *a, struct vec2d *b)
        + a->y * b->y;
 }
 
+double
+dotp3d(struct vec3d *a, struct vec3d *b)
+{
+  return a->x * b->x
+       + a->y * b->y
+       + a->z * b->z;
+}
+
 void
 sum2d(struct vec2d *a, struct vec2d *b, struct vec2d *dst)
 {
   dst->x = a->x + b->x;
   dst->y = a->y + b->y;
+}
+
+void
+sum3d(struct vec3d *a, struct vec3d *b, struct vec3d *dst)
+{
+  dst->x = a->x + b->x;
+  dst->y = a->y + b->y;
+  dst->z = a->z + b->z;
 }
 
 void
@@ -24,10 +40,26 @@ sub2d(struct vec2d *a, struct vec2d *b, struct vec2d *dst)
 }
 
 void
+sub3d(struct vec3d *a, struct vec3d *b, struct vec3d *dst)
+{
+  dst->x = a->x - b->x;
+  dst->y = a->y - b->y;
+  dst->z = a->z - b->z;
+}
+
+void
 scalarmult2d(double c, struct vec2d *a, struct vec2d *dst)
 {
   dst->x = c * a->x;
   dst->y = c * a->y;
+}
+
+void
+scalarmult3d(double c, struct vec3d *a, struct vec3d *dst)
+{
+  dst->x = c * a->x;
+  dst->y = c * a->y;
+  dst->z = c * a->z;
 }
 
 void
@@ -37,21 +69,34 @@ scalardiv2d(double c, struct vec2d *a, struct vec2d *dst)
   dst->y = a->y / c;
 }
 
+void
+scalardiv3d(double c, struct vec3d *a, struct vec3d *dst)
+{
+  dst->x = a->x / c;
+  dst->y = a->y / c;
+  dst->z = a->z / c;
+}
+
 double
 norm2d(struct vec2d *v)
 {
   return pow(dotp2d(v, v), 0.5);
 }
 
-// FIXME: compatibility name. delete this
-void
-assign2d(struct vec2d *dst, struct vec2d *src)
+double
+norm3d(struct vec3d *v)
 {
-  vec2d_assign(dst, src);
+  return pow(dotp3d(v, v), 0.5);
 }
 
 void
-vec2d_assign(struct vec2d *dst, struct vec2d *src)
+assign2d(struct vec2d *dst, struct vec2d *src)
+{
+  *dst = *src;
+}
+
+void
+assign3d(struct vec3d *dst, struct vec3d *src)
 {
   *dst = *src;
 }
--- maths.h
+++ maths.h
@@ -5,31 +5,41 @@
 
 double
 dotp2d(struct vec2d *, struct vec2d *);
+double
+dotp3d(struct vec3d *, struct vec3d *);
 
 void
 sum2d(struct vec2d *, struct vec2d *, struct vec2d *);
+void
+sum3d(struct vec3d *, struct vec3d *, struct vec3d *);
 
 void
 sub2d(struct vec2d *, struct vec2d *, struct vec2d *);
+void
+sub3d(struct vec3d *, struct vec3d *, struct vec3d *);
 
 void
 scalarmult2d(double, struct vec2d *, struct vec2d *);
+void
+scalarmult3d(double, struct vec3d *, struct vec3d *);
 
 void
 scalardiv2d(double, struct vec2d *, struct vec2d *);
+void
+scalardiv3d(double, struct vec3d *, struct vec3d *);
 
 double
 norm2d(struct vec2d *);
+double
+norm3d(struct vec3d *);
 
 void
 assign2d(struct vec2d *, struct vec2d *);
 void
-vec2d_assign(struct vec2d *, struct vec2d *);
-void
-vec3d_assign(struct vec3d *, struct vec3d *);
+assign3d(struct vec3d *, struct vec3d *);
 
 void
-mat2d_mult(struct mat2d *, struct mat2d *, struct mat2d *);
+multmat2d(struct mat2d *, struct mat2d *, struct mat2d *);
 
 
 #endif // MATHS_H
